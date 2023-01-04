3e30cd26af90a14168becec06554ed100e12cef5
dhruvstrats360 <dhruvchhatbar@strats360.com>
2023-01-04 18:14:06 +0530

    Initial Commit

---
+++ Strats360_KidsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Strats360_KidsApp
+//
+//  Created by Strats 360 on 04/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
